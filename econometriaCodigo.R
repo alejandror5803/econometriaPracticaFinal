a721f0f81dbc647c24be127a2bab369c2b643db4
alejandror5803 <115725585+alejandror5803@users.noreply.github.com>
2026-04-26 14:53:20 +0200

    update

--- econometriaCodigo.R
+++ econometriaCodigo.R
@@ -1,7 +1,6 @@
 # 0) Leer el fichero de datos
 raw <- read.csv("idealista_es_sale_homes_madrid_pages84_2025-07-15.csv")
-# Sustituir "Madrid" por la cadena EXACTA del municipio asignado a vuestro grupo
-chosen_muni <- "Madrid"
+chosen_muni <- "Alcobendas"
 # Conservar solo vuestro municipio y las ventas
 dat <- subset(raw, municipality == chosen_muni & operation == "sale")
 # Conservar ´unicamente las columnas seleccionadas (no renombrar)
@@ -30,3 +29,31 @@ boxplot(price ~ propertyType, data=dat, main="Precio por tipo de propiedad", las
 
 # 2) regresiones simples
 
+# Tama~no y precio
+m1 <- lm(price ~ size, data=dat) # precio sobre tama~no
+m2 <- lm(log_price ~ log_size, data=dat) # elasticidad del precio respecto al tama~no
+# Habitaciones y ba~nos
+m3 <- lm(log_price ~ rooms, data=dat) # efecto de las habitaciones
+m4 <- lm(log_price ~ bathrooms, data=dat) # efecto de los ba~nos
+
+# Tipo de propiedad y estado
+m5 <- lm(price ~ propertyType, data=dat)
+m6 <- lm(price ~ status, data=dat)
+summary(m1); summary(m2); summary(m3)
+summary(m4); summary(m5); summary(m6)
+
+# 3) regresion multivariante
+full <- lm(log_price ~ log_size + rooms + bathrooms + propertyType + status, data=dat)
+summary(full)
+
+
+reduced <- lm(log_price ~ log_size + rooms + bathrooms + status, data=dat)
+anova(reduced, full) # F-test para propertyType
+
+
+
+# 4) Forma funcional y extensiones
+nl <- lm(log_price ~ log_size + I(log_size^2) + rooms + bathrooms +
+           propertyType + status,
+         data=dat)
+summary(nl)
